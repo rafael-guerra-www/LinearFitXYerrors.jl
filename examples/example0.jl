d2aa20e679be7b0f07e8ee378fa87a5fae9fc557
Rafael Guerra <rafael.guerra.www@gmail.com>
2021-09-09 02:34:37 +0300

    plots

--- examples/example0.jl
+++ examples/example0.jl
@@ -9,11 +9,11 @@ using LinearFitXYerrors
 X = [91., 104, 107, 107, 106, 100, 92, 92, 105, 108]
 Y = [9.8, 7.4, 7.9, 8.3, 8.3, 9.0, 9.7, 8.8, 7.6, 6.9]
 
-σX =  σY = r = 0
+# σX =  σY = r = 0
 
 
 # COMPUTE and PLOT:
-stxy = linearfitxy(X,Y, σX=σX, σY=σY, r=r, isplot=true)
+stxy = linearfitxy(X,Y, isplot=true)
 
 
 ########################################
--- src/linearfitxy.jl
+++ src/linearfitxy.jl
@@ -3,7 +3,7 @@
 # set plotting defaults
 plot_font = "Computer Modern";
 default(fontfamily=plot_font,framestyle=:axes,yminorgrid=true, legendtitlefontsize=6,fg_color_legend=nothing,
-    legendfontsize=6, guidefont=(7,:black),tickfont=(6,:black),size=(600,400),dpi=300, margin=0mm,
+    legendfontsize=6, guidefont=(7,:black),tickfont=(6,:black),size=(600,400), dpi=300, margin=0mm,
     titlefont = (6, plot_font))
 
 struct stfit
