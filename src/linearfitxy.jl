df9c87a5930e42c7b84605ef298280a9d3826602
Rafael Guerra <rafael.guerra.www@gmail.com>
2021-09-09 22:59:54 +0300

    Update linearfitxy.jl

--- src/linearfitxy.jl
+++ src/linearfitxy.jl
@@ -31,10 +31,10 @@ Performs 1D linear fitting of experimental data with uncertainties in  X and Y:
 # Arguments:
 - `X` and `Y` are input data vectors with length ≥ 3
 - Optional standard deviation errors ``σX`` and ``σY`` are vectors or scalars
-- Optional `r` is the correlation between the ``σX`` and ``σY`` errors\
+- Optional `r` is the correlation between the ``σX`` and ``σY`` errors
 `r` can be a vector or scalar
 
-For the ``σX`` and ``σY`` errors (error ellipses) a bivariate Gaussian distribution is assumed.\
+For the ``σX`` and ``σY`` errors (error ellipses) a bivariate Gaussian distribution is assumed.
 If no errors are provided, or if only ``σX`` or ``σY`` are provided, then the results are equivalent
 to those obtained using the [LsqFit.jl](https://github.com/JuliaNLSolvers/LsqFit.jl) package.
 
@@ -49,17 +49,17 @@ st = linearfitxy(X, Y; σX, σY, isplot=true)    # XY errors not correlated (r=0
 st = linearfitxy(X, Y; σX, σY, r=0, isplot=true, ratio=:auto) # XY errors not correlated (r=0); plot result
 ```
 
-The results are in the fields of the returned st::stfitxy structure.:
+The results are in the fields of the returned st::stfitxy structure:
 - The intercept `a`, the slope `b` and their uncertainties `σa` and `σb`
 - ``σa95`` and ``σb95``: 95%-confidence interval using two-tailed t-Student distribution,
     e.g.: ``b ± σb95 = b ± t(0.975,N-2)*σb``
-- Goodness of fit `S` (reduced ``Χ²`` test): underlying quantity has ``Χ²`` N-2 degrees of freedom\
+- Goodness of fit `S` (reduced ``Χ²`` test): underlying quantity has ``Χ²`` N-2 degrees of freedom
   `S ~ 1`: fit consistent with errors, `S > 1`: poor fit, `S >> 1`: errors underestimated,
   `S < 1`: overfitting or errors overestimated
 - Pearson's correlation coefficient ``ρ`` that accounts for data errors
 - Optional display of fit results with error ellipses and confidence intervals
 
-The default argument `isplot=false` can be turned on to plot the results.\
+The default argument `isplot=false` can be turned on to plot the results.
 Currently Plots.jl's gr() is used
 
 """
